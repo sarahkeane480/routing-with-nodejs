d0ca0700a8773c8f816f038efec87765869005dc
Replit user <>
2023-08-08 16:34:33 +0000

    new stuff

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
+http.createServer(function (req, res) {
+  var path = req.url.toLowerCase();
+
+  switch (path) {
+    case "/": // The default path
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Aloha world");
+      break;
+
+    case "/about": // The default path
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("About me");
+      break;
+
+    default: // Random paths you shouldn't go to!!
+      // The classic 404 message we all know and love!
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("ERROR !! Not found");
+      // break;
+  }
+
+  // res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // res.end('Aloha world');
 }).listen(process.env.PORT || 3000);
